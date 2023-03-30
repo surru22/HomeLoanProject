b35a81d4a37eeb7e37c1d5d618950fd50e46cab8
surru22 <129243631+surru22@users.noreply.github.com>
2023-03-30 16:21:31 +0530

    changes
    changes

---
+++ src/main/java/com/securehome/main/servicei/LedgerServiceI.java
@@ -0,0 +1,8 @@
+package com.SecureHome.main.servicei;
+
+import com.SecureHome.main.model.Ledger;
+
+public interface LedgerServiceI {
+
+	public Ledger storeLedger(Ledger l);
+}
---
+++ src/main/java/com/securehome/main/servicei/PreviousBankDetailsServiceI.java
@@ -0,0 +1,9 @@
+package com.SecureHome.main.servicei;
+
+import com.SecureHome.main.model.PreviousBankDetails;
+
+public interface PreviousBankDetailsServiceI {
+
+	public PreviousBankDetails storePreviousBankDetails(PreviousBankDetails pbd);
+
+}
