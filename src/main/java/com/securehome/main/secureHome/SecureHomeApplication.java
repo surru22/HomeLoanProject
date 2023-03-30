2989c90d9d56918a04da8888f0064f9b9e01f127
LuffyEmma <koundalkaryogesh4@gmail.com>
2023-03-30 16:03:44 +0530

    first commit

---
+++ src/main/java/com/securehome/main/secureHome/SecureHomeApplication.java
@@ -0,0 +1,13 @@
+package com.securehome.main.secureHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securehome/main/secureHome/SecureHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securehome.main.secureHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
